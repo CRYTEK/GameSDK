ae94b3b4f8cd9d2e5ce005e1f9e029308839a5b3
Marek Zeman <marekz@crytek.com>
2017-02-02 15:58:36 +0100

    !T (DEV-3048) Changes regarding to moving PerceptionManager from AI system to plugin
    Copied from Perforce
     Change: 1487249

--- Scripts/AI/aiconfig.lua
+++ Scripts/AI/aiconfig.lua
@@ -17,44 +17,9 @@ Script.ReloadScript("Scripts/AI/Formations/FormationManager.lua");
 Script.ReloadScript("Scripts/AI/Logic/AI_Utils.lua");
 Script.ReloadScript("SCRIPTS/AI/Logic/BlackBoard.lua");
 
-function ReloadTPS()
-	AI.DestroyAllTPSQueries();
-	Script.ReloadScript("Scripts/AI/TacticalPositionManager.lua");
-	AI.TacticalPositionManager:OnInit();
-end
-
-function ReloadTPSExtensions()
-	ReloadTPS();
-	
-	if (PipeManager) then
-		PipeManager:OnInit();
-	end
-end
-
-function AIReload()
-	ReloadTPSExtensions();
-	assignmentsHaveBeenCreated = nil
-end
-
-if (FormationManager) then
-	FormationManager:OnInit();
-end
-
--- SETTING UP THE PLAYER ASSESMENT MULTIPLIER
--- should be in the range [0,1] BUT can be bigger - forcing something be very important (grenades)
--- if multiplayer is more than 1, object will not be ignored with "igoreall" goal
-AI.SetAssesmentMultiplier(AIOBJECT_PLAYER,1.0);	-- let's make squadmqtes prefferd enemies
-AI.SetAssesmentMultiplier(AIOBJECT_ATTRIBUTE,1.0);
-AI.SetAssesmentMultiplier(AIOBJECT_RPG, 100); -- grenade is very important
-
---------------------------------------------------
 --define combat classes
 AICombatClasses = {};
 
---this will reset all existing combat classes
-AI.AddCombatClass();	
-
---	ATTENTION!!! DON'T make the scale multiplyer 0 unlees you really need this class to be ignored
 AICombatClasses.Player = 0;
 AICombatClasses.PlayerRPG = 1;
 AICombatClasses.Infantry = 2;
@@ -75,33 +40,78 @@ AICombatClasses.Scout = 16;
 AICombatClasses.ascensionScout = 17;
 AICombatClasses.ascensionVTOL = 18;
 AICombatClasses.ascensionScout2 = 19;
-																							--		0			1			2			3			4			5			6			7			8			9			10		11		12		13		14		15		16		17		18		19
-AI.AddCombatClass(AICombatClasses.Player,					{	1.0,	1.0,	1.0,	1.0,	1.1,	1.1,	1.0,	1.0,	2.0,	0.0,	0.0,	0.0,	1.0,	1.0,	1.0,	0.1,	1.0,	1.0,	0.0,	0.0,   } );																								
-AI.AddCombatClass(AICombatClasses.PlayerRPG,			{	1.0,	1.0,	1.0,	1.0,	1.5,	1.5,	1.0,	1.0,	2.0,	0.0,	1.2,	0.0,	1.2,	1.0,	1.2,	0.1,	1.0,	1.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.Infantry,				{	1.0,	1.0,	1.0,	1.0,	1.1,	1.1,	1.0,	1.0,	2.0,	0.0,	0.0,	0.0,	1.0,	1.0,	1.0,	0.1,	1.0,	1.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.InfantryRPG,		{	1.0,	1.0,	1.0,	1.0,	1.5,	1.5,	1.0,	1.0,	2.0,	0.0,	1.2,	0.0,	1.2,	1.0,	1.2,	0.1,	1.0,	1.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.Tank,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
-AI.AddCombatClass(AICombatClasses.TankHi,					{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
-AI.AddCombatClass(AICombatClasses.Heli,						{	1.0,	1.0,	1.0,	1.0,	0.9,	0.9,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	0.9,	1.0,	0.9,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnHeliSeen" );	
-AI.AddCombatClass(AICombatClasses.VehicleGunner,	{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   } );	-- for the vehicle gunner. 1.5 means, I want to set the same priority as the player.
-AI.AddCombatClass(AICombatClasses.Hunter,					{	1.0,	1.2,	0.8,	1.0,	2.0,	2.0,	2.0,	1.0,	2.0,	0.0,	1.0,	0.0,	2.0,	2.0,	2.0,	0.8,	1.5,	0.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.Civilian,				{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	0.5,	0.0,	1.0,	1.0,	1.0,	0.1,	1.0,	0.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.Car,						{	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.Warrior,				{	0.05,	0.2,	0.05,	0.2,	0.2,	1.5,	2.0,	1.5,	0.05,	0.05,	1.5,	1.5,	1.5,	0.2,	1.5,	0.05,	0.05,	0.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.AAA,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
-AI.AddCombatClass(AICombatClasses.BOAT,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnBoatSeen" );	
-AI.AddCombatClass(AICombatClasses.APC,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
-AI.AddCombatClass(AICombatClasses.Squadmate,			{	1.0,	1.0,	1.0,	1.0,	1.1,	1.1,	1.0,	1.0,	2.0,	0.0,	0.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.Scout,					{	1.0,  1.0,  0.7,	0.7,	0.9,	0.9,	1.0,	0.7,	1.0,	0.0,	1.0,	0.0,	0.9,	0.9,	0.9,	0.7,	1.0,	1.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.ascensionScout,	{	1.0,	1.0,	0.0,	0.5,	0.5,  0.0,	1.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.ascensionVTOL,	{	1.0,	1.0,	0.0,	0.0,	0.0, 	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
-AI.AddCombatClass(AICombatClasses.ascensionScout2,{	1.0,	1.0,	0.0,	0.7,	0.7,  0.0,	1.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
 
+--------------------------------------------------
+
+function InitCombatClasses()	
+	--this will reset all existing combat classes
+	AI.AddCombatClass();
+																								--		0			1			2			3			4			5			6			7			8			9			10		11		12		13		14		15		16		17		18		19
+	--	ATTENTION!!! DON'T make the scale multiplyer 0 unlees you really need this class to be ignored
+	AI.AddCombatClass(AICombatClasses.Player,					{	1.0,	1.0,	1.0,	1.0,	1.1,	1.1,	1.0,	1.0,	2.0,	0.0,	0.0,	0.0,	1.0,	1.0,	1.0,	0.1,	1.0,	1.0,	0.0,	0.0,   } );																								
+	AI.AddCombatClass(AICombatClasses.PlayerRPG,			{	1.0,	1.0,	1.0,	1.0,	1.5,	1.5,	1.0,	1.0,	2.0,	0.0,	1.2,	0.0,	1.2,	1.0,	1.2,	0.1,	1.0,	1.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.Infantry,				{	1.0,	1.0,	1.0,	1.0,	1.1,	1.1,	1.0,	1.0,	2.0,	0.0,	0.0,	0.0,	1.0,	1.0,	1.0,	0.1,	1.0,	1.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.InfantryRPG,		{	1.0,	1.0,	1.0,	1.0,	1.5,	1.5,	1.0,	1.0,	2.0,	0.0,	1.2,	0.0,	1.2,	1.0,	1.2,	0.1,	1.0,	1.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.Tank,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
+	AI.AddCombatClass(AICombatClasses.TankHi,					{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
+	AI.AddCombatClass(AICombatClasses.Heli,						{	1.0,	1.0,	1.0,	1.0,	0.9,	0.9,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	0.9,	1.0,	0.9,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnHeliSeen" );	
+	AI.AddCombatClass(AICombatClasses.VehicleGunner,	{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   } );	-- for the vehicle gunner. 1.5 means, I want to set the same priority as the player.
+	AI.AddCombatClass(AICombatClasses.Hunter,					{	1.0,	1.2,	0.8,	1.0,	2.0,	2.0,	2.0,	1.0,	2.0,	0.0,	1.0,	0.0,	2.0,	2.0,	2.0,	0.8,	1.5,	0.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.Civilian,				{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	0.5,	0.0,	1.0,	1.0,	1.0,	0.1,	1.0,	0.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.Car,						{	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.Warrior,				{	0.05,	0.2,	0.05,	0.2,	0.2,	1.5,	2.0,	1.5,	0.05,	0.05,	1.5,	1.5,	1.5,	0.2,	1.5,	0.05,	0.05,	0.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.AAA,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
+	AI.AddCombatClass(AICombatClasses.BOAT,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnBoatSeen" );	
+	AI.AddCombatClass(AICombatClasses.APC,						{	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	1.0,   }, "OnTankSeen" );	
+	AI.AddCombatClass(AICombatClasses.Squadmate,			{	1.0,	1.0,	1.0,	1.0,	1.1,	1.1,	1.0,	1.0,	2.0,	0.0,	0.0,	0.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.Scout,					{	1.0,  1.0,  0.7,	0.7,	0.9,	0.9,	1.0,	0.7,	1.0,	0.0,	1.0,	0.0,	0.9,	0.9,	0.9,	0.7,	1.0,	1.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.ascensionScout,	{	1.0,	1.0,	0.0,	0.5,	0.5,  0.0,	1.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.ascensionVTOL,	{	1.0,	1.0,	0.0,	0.0,	0.0, 	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
+	AI.AddCombatClass(AICombatClasses.ascensionScout2,{	1.0,	1.0,	0.0,	0.7,	0.7,  0.0,	1.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0,   } );	
+end
+
+function ReloadTPS()
+	AI.DestroyAllTPSQueries();
+	Script.ReloadScript("Scripts/AI/TacticalPositionManager.lua");
+	AI.TacticalPositionManager:OnInit();
+end
+
+function ReloadTPSExtensions()
+	ReloadTPS();
+	
+	if (PipeManager) then
+		PipeManager:OnInit();
+	end
+end
+
+--------------------------------------------------
+function AIInit()
+	if (FormationManager) then
+		FormationManager:OnInit();
+	end
+
+	InitCombatClasses();
+end
+
+-- call AIInit by default
+AIInit();
 
 function AIReset()
 	AIBlackBoard_Reset();
 	spectacularKillInProgress = false
 	lastWalkabilityCheckTime = false
+end
+
+function AIReload()
+	ReloadTPSExtensions();
+	assignmentsHaveBeenCreated = nil
+end
+
+function AI:AIPostInit()
+	-- all plugins are loaded at this point
+	Perception.SetPriorityObjectType(AIOBJECT_PLAYER);	-- let's make squadmates preffered enemies
+	Perception.SetPriorityObjectType(AIOBJECT_ATTRIBUTE);
+	Perception.SetPriorityObjectType(AIOBJECT_RPG); -- grenade is very important
 end
 
 function AI:OnSave(save)
